f45bc2c31a66147b057033e835d519c39eb7cc9e
ilikka <47757386+ilikka@users.noreply.github.com>
2025-12-28 16:28:32 +0200

    1st real payload demo

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://user:password@localhost:5432/ruuvi"
+from config import DATABASE_URL
+
+
 
 engine = create_engine(DATABASE_URL, future=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
--- app/ruuvi_api.py
+++ app/ruuvi_api.py
@@ -16,27 +16,33 @@ async def receive_ruuvi(request: Request, db: Session = Depends(get_db)):
   body = await request.json()
   logger.debug("Received payload: %s", body)
 
-  if "tag_mac" not in body:
+  data = body.get("data", {})
+  gw_mac = data.get("gw_mac")
+  tags = data.get("tags", [])
+    
+  if not gw_mac or not tags:
     logger.warning("Invalid payload: %s", body)
     return {"error": "Invalid payload"}
 
 
-  gw_mac = body.get("gw_mac")
-  tag_mac = body.get("tag_mac")
-  rssi = body.get("rssi")
-  raw_data = body.get("raw_data")
-  temperature = body.get("temperature")
-  humidity = body.get("humidity")
-  pressure = body.get("pressure")
-  battery = body.get("battery")
+  for tag_mac, tag_data in tags.items():
+    rssi = tag_data.get("rssi")
+    temperature = tag_data.get("temperature")
+    humidity = tag_data.get("humidity")
+    pressure = tag_data.get("pressure")
+    voltage = tag_data.get("voltage")
 
-  if not gw_mac or not tag_mac:
-    return {"error": "Missing gw_mac or tag_mac"}
+    logger.info(
+      "gw=%s tag=%s temp=%.2f hum=%.2f",
+      gw_mac, tag_mac, temperature, humidity
+    )
 
+
+  
   # Run synchronous DB calls in a thread to avoid async errors
-  await asyncio.to_thread(crud.get_or_create_gateway, db, gw_mac)
-  await asyncio.to_thread(crud.get_or_create_sensor, db, tag_mac)
-  await asyncio.to_thread(crud.save_raw, db, gw_mac, tag_mac, raw_data, rssi)
-  await asyncio.to_thread(crud.save_data, db, gw_mac, tag_mac, temperature, humidity, pressure, battery, rssi)
+  # await asyncio.to_thread(crud.get_or_create_gateway, db, gw_mac)
+  # await asyncio.to_thread(crud.get_or_create_sensor, db, tag_mac)
+  # await asyncio.to_thread(crud.save_raw, db, gw_mac, tag_mac, raw_data, rssi)
+  #await asyncio.to_thread(crud.save_data, db, gw_mac, tag_mac, temperature, humidity, pressure, battery, rssi)
 
   return {"status": "ok"}
