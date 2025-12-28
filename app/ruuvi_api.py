9e839b8c9cb89cefff2863122fd862a5dba53e97
ilikka <47757386+ilikka@users.noreply.github.com>
2025-12-28 10:10:47 +0200

    debug app

--- app/ruuvi_api.py
+++ app/ruuvi_api.py
@@ -1,52 +1,33 @@
 # ruuvi_api.py
 from fastapi import FastAPI, Request
 from ruuvi_decoders import Decoder
+from logging_config import setup_logging
+import logging
+
 import logging
 import psycopg2
 import os
+import logging
+
+# uvicorn ruuvi_api:app --host 0.0.0.0 --port 40064
+
+setup_logging()
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 decoder = Decoder()
 
-# Connect to PostgreSQL
-conn = psycopg2.connect(
-    host=os.environ.get("PG_HOST", "localhost"),
-    dbname=os.environ.get("PG_DB", "ruuvi"),
-    user=os.environ.get("PG_USER", "ruuvi_user"),
-    password=os.environ.get("PG_PASS", "secret")
-)
-cur = conn.cursor()
+
 
 @app.post("/ruuvi")
 async def receive_ruuvi(request: Request):
     body = await request.json()
-    
+    logger.info("body : %s", body)    
     if "data" not in body:
         return {"error": "Invalid payload"}
     
-    gw_mac = body["data"].get("gwmac", "")
-    tags = body["data"].get("tags", {})
 
-    for tag_mac, tag in tags.items():
-        rssi = tag.get("rssi")
-        raw_data = tag.get("data", "")
-        
-        # Decode Ruuvi sensor data
-        try:
-            decoded = decoder.decode_raw(raw_data)
-        except Exception:
-            decoded = {}
-        
-        temperature = decoded.get("temperature")
-        humidity = decoded.get("humidity")
-        pressure = decoded.get("pressure")
-        battery = decoded.get("battery")
-            
-        # Insert into PostgreSQL
-        cur.execute("""
-            INSERT INTO ruuvi_data(gw_mac, tag_mac, temperature, humidity, pressure, battery, rssi)
-            VALUES (%s, %s, %s, %s, %s, %s, %s)
-        """, (gw_mac, tag_mac, temperature, humidity, pressure, battery, rssi))
-        
-    conn.commit()
     return {"status": "ok"}
+
+
+
