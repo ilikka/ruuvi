8e742363bbff93067ef2b976470a710c56df8b7f
ilikka <47757386+ilikka@users.noreply.github.com>
2025-12-28 10:17:07 +0200

    logging

--- app/ruuvi_api.py
+++ app/ruuvi_api.py
@@ -1,8 +1,6 @@
 # ruuvi_api.py
 from fastapi import FastAPI, Request
 # ßfrom ruuvi_decoders import decode_data as Decoder
-from logging_config import setup_logging
-import logging
 
 import logging
 import psycopg2
@@ -11,11 +9,10 @@ import logging
 
 # uvicorn ruuvi_api:app --host 0.0.0.0 --port 40064
 
-setup_logging()
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
-decoder = Decoder()
+# decoder = Decoder()
 
 
 
